887786ae762ffc22d21e36e295732176d6b77bf2
Ati <190020009@iitdh.ac.in>
2021-07-08 16:55:47 +0530

    home and about configured

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -111,16 +111,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="https://blogs.soumyajit.tech/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/soumyajit4419/Portfolio"
