60fca873f293effb1a78e4a7ad3c971059f8f077
atul singh <atul.singh@STS-2758.sts.co.in>
2024-08-16 06:31:35 +0530

    source path for homepage

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
